366df7b6c5459111ea1e849587b0a1a9414affc0
Dineshyadav3505 <dineshyadav20001210@gmail.com>
2024-08-09 13:09:48 +0530

    Refactor image positioning in Card components

--- src/components/Card/BeveragesCard.js
+++ src/components/Card/BeveragesCard.js
@@ -26,14 +26,14 @@ const BeveragesCard = ({ product }) => {
     return (
         <div className="border w-full mx-auto my-5 md:w-60 md:h-[420px] lg:w-72 h-[425px] lg:h-[420px] border-zinc-900 dark:border-zinc-100 rounded-lg p-2 shadow-md">
             <div className='relative flex-wrap'>
-                <div className="h-48 w-full rounded-md overflow-hidden">
+                <div className="h-48 w-full relative rounded-md overflow-hidden">
                     <img
                         src={product.image} 
                         alt={product.name} 
                         className="h-full w-full object-cover rounded-md" 
                     />
+                    <p className="text-base mt-2 absolute bottom-2 left-1 text-white bg-black px-2 py-1">₹ {totalPrice}</p>
                 </div>
-                <p className="text-base mt-2 absolute top-36 left-1 text-white bg-black px-2 py-1">₹ {totalPrice}</p>
                 <h3 className="text-base font-semibold py-1">{product.name}</h3>
                 <p className="text-zinc-500 text-sm h-16 overflow-hidden">{product.description}</p>            
             </div>
